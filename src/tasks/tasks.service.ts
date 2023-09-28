86e3cc3b56f4ba42204321e6de1aa8ee94a2df41
huyle3 <lehuy3@hitachivantara.com>
2023-09-28 09:01:42 +0700

    check id null

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
 import { getTaskFilterDTO } from './dto/getTaskFilterDTO';
@@ -29,7 +29,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task ${id} not found`);
+    }
+    return found;
   }
 
   createTask(createTaskDTO): Task {
